fd202a06ae66c5c04a664085ec84606aa9302004
Mauricio Sierra <mauricio.sierra@gmail.com>
2018-03-11 19:00:34 -0400

    add basic empty tests

---
+++ todo/app/src/androidTest/java/tokotlin/todo/list/ToDoListFragmentTest.java
@@ -0,0 +1,17 @@
+package tokotlin.todo.list;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ToDoListFragmentTest {
+
+    @Test
+    public void testListFragmentDisplay() {
+    }
+
+}
---
+++ todo/app/src/test/java/tokotlin/todo/create/CreateToDoViewModelTest.java
@@ -0,0 +1,14 @@
+package tokotlin.todo.create;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CreateToDoViewModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+}
