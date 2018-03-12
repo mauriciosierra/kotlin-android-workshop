c0660813c5c2818367cf57ad852f8a505b8a5775
Mauricio Sierra <mauricio.sierra@gmail.com>
2018-03-11 20:14:42 -0400

    convert list package to kotlin

--- todo/app/src/main/kotlin/tokotlin/todo/list/ToDoListFragment.kt
+++ todo/app/src/main/kotlin/tokotlin/todo/list/ToDoListFragment.kt
@@ -2,24 +2,19 @@ package tokotlin.todo.list
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import java.util.Collections
-
 import tokotlin.todo.R
 import tokotlin.todo.model.ToDo
 
 class ToDoListFragment : Fragment() {
 
-    private var viewModel: ToDoListViewModel? = null
-
-    private val observer = Observer<List<ToDo>> { toDos -> updateToDoItems(view!!.findViewById<View>(R.id.todo_list) as RecyclerView, toDos) }
+    private lateinit var viewModel: ToDoListViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -28,13 +23,15 @@ class ToDoListFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = ToDoRecyclerViewAdapter(listOf())
 
-        viewModel = ViewModelProviders.of(this).get(ToDoListViewModel::class.java!!)
-        viewModel!!.toDoList.observe(this, observer)
+        viewModel = ViewModelProviders.of(this).get(ToDoListViewModel::class.java)
+        viewModel.toDoList.observe(this, Observer { toDos -> toDos?.apply {
+            updateToDoItems(view.findViewById(R.id.todo_list), toDos) }
+        })
 
         return view
     }
 
-    private fun updateToDoItems(recyclerView: RecyclerView, toDoItems: List<ToDo>?) {
+    private fun updateToDoItems(recyclerView: RecyclerView, toDoItems: List<ToDo>) {
         (recyclerView.adapter as ToDoRecyclerViewAdapter).setToDoList(toDoItems)
     }
 }
---
+++ todo/app/src/main/kotlin/tokotlin/todo/list/ToDoRecyclerViewAdapter.kt
@@ -0,0 +1,38 @@
+package tokotlin.todo.list
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import tokotlin.todo.R
+import tokotlin.todo.model.ToDo
+
+class ToDoRecyclerViewAdapter(private var toDoList: List<ToDo>?) : RecyclerView.Adapter<ToDoRecyclerViewAdapter.ToDoViewHolder>() {
+
+    fun setToDoList(toDoList: List<ToDo>) {
+        this.toDoList = toDoList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        return ToDoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.todo_list_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        holder.bindToDoItem(toDoList?.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return toDoList?.size ?: 0
+    }
+
+    class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindToDoItem(toDo: ToDo?) {
+            itemView.findViewById<TextView>(R.id.title).text = toDo?.title ?: ""
+            itemView.findViewById<TextView>(R.id.description).text = toDo?.description ?: ""
+        }
+    }
+}
